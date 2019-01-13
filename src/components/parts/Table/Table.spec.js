146193ea4bd834b82941256e594f44283848e223
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2019-01-13 15:48:01 -0600

    feat: added new Table part

---
+++ src/components/parts/Table/Table.spec.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import {
+  shallow,
+} from 'enzyme'
+import _ from 'lodash'
+
+import {
+  Table,
+  actions,
+  selectors,
+} from './Table'
+
+// import stylesClass from './Table.scss'
+// const styles = _.mapValues(stylesClass, (raw) => '.' + raw)
+
+export const Table_specs = describe('<Table />', () => {
+  let sandbox
+  let props
+  let prop_actions
+  let prop_selectors
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create()
+
+    require('react-proptype-error-catcher')(sandbox)
+
+    prop_selectors = {
+      active_player: 'host',
+      can_deal: true,
+      dealer_deck: [],
+      board_cards: [],
+      host_cards: [],
+      friend_cards: [],
+    }
+    prop_actions = {
+      select_card: sandbox.stub(),
+      deal_cards: sandbox.stub(),
+    }
+
+    props = {
+      ...prop_selectors,
+      ...prop_actions,
+    }
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  describe('general', () => {
+    it('with normal props it should render without errors', () => {
+      const wrapper = shallow(<Table {...props} />)
+      expect(wrapper).to.exist
+    })
+    it('redux actions for connect should be valid and complete', () => {
+      if (_.keys(prop_actions).length) {
+        expect(actions).to.contain.all.keys(prop_actions)
+        _.forEach(actions, (actionFn, key) => {
+          expect(actionFn, `name: ${key}`).to.be.a('function')
+        })
+      }
+    })
+    it('redux selectors for connect should be valid and complete', () => {
+      if (_.keys(prop_selectors).length) {
+        expect(selectors).to.contain.all.keys(prop_selectors)
+        _.forEach(selectors, (selectorFn, key) => {
+          expect(selectorFn, `name: ${key}`).to.be.a('function')
+        })
+      }
+    })
+  })
+  describe('behaviors', () => {
+    it('if active_player is host set first PlayerWinnings to is_active', () => {
+      props.active_player = 'host'
+      const wrapper = shallow(<Table {...props} />)
+      expect(wrapper.find('PlayerWinnings').at(0)).to.have.prop('is_active').eql(true)
+      expect(wrapper.find('PlayerWinnings').at(1)).to.have.prop('is_active').eql(false)
+    })
+    it('if active_player is friend set second PlayerWinnings to is_active', () => {
+      props.active_player = 'friend'
+      const wrapper = shallow(<Table {...props} />)
+      expect(wrapper.find('PlayerWinnings').at(0)).to.have.prop('is_active').eql(false)
+      expect(wrapper.find('PlayerWinnings').at(1)).to.have.prop('is_active').eql(true)
+    })
+  })
+})
---
+++ src/components/parts/Table/Table.stories.js
@@ -0,0 +1,37 @@
+/* eslint no-magic-numbers: 0 */
+/* eslint max-len: 0 */
+import React from 'react'
+import {knob, stories} from 'helpers/storybook_helpers'
+import _ from 'lodash'
+// import faker from 'faker'
+// import dayjs from 'dayjs'
+
+const TOTAL_CARDS = 24
+
+stories('Parts', module, {width: '100%', height: '100%'})
+  .spec_and_info(
+    'Table',
+    {
+      spec: require('./Table.spec').Table_specs,
+      Component: require('./Table').Table,
+      render: (Table) => {
+        const cards = _.times(TOTAL_CARDS, (id) => ({
+          id: String(id),
+          image_name: String(Math.floor(id / 2)),
+          show_front: false,
+        }))
+        return () => (
+          <Table
+            active_player={knob.boolean('active_player is host', true) ? 'host' : 'friend'}
+            can_deal={knob.boolean('can_deal', true)}
+            dealer_deck={cards}
+            board_cards={cards}
+            host_cards={cards}
+            friend_cards={cards}
+            select_card={knob.action('select_card')}
+            deal_cards={knob.action('deal_cards')}
+          />
+        )
+      },
+    }
+  )
